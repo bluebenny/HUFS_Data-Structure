788a7957f6b7bdd686ec5a052c7a356cec92a9a1
benny <benny-kim@naver.com>
2023-05-14 21:26:44 +0900

    assignment 5

--- assignment/src/main/java/assignment/assignment5/Assignment5_3.java
+++ assignment/src/main/java/assignment/assignment5/Assignment5_3.java
@@ -4,18 +4,29 @@ import java.util.Scanner;
 
 class Point {
 	public int x, y;
+
+	public Point() {
+		x = 0;
+		y = 0;
+	}
 }
 
 class Status {
 	public int x, y;
 	public int position;
+
+	public Status() {
+		x = 0;
+		y = 0;
+		position = 0;
+	}
 }
 
 public class Assignment5_3 {
-    // 필요한 메소드가 있으면 작성하시오.
+	// 필요한 메소드가 있으면 작성하시오.
 
 	static int xMove(int position) {
-		switch(position) {
+		switch (position) {
 			case 0:
 				return -1;
 			case 2:
@@ -26,7 +37,7 @@ public class Assignment5_3 {
 	}
 
 	static int yMove(int position) {
-		switch(position) {
+		switch (position) {
 			case 3:
 				return -1;
 			case 1:
@@ -35,32 +46,32 @@ public class Assignment5_3 {
 				return 0;
 		}
 	}
-	
+
 	public static void main(String[] args) throws Exception {
 		Scanner scan = new Scanner(System.in);
-		int sizeX = scan.nextInt();
 		int sizeY = scan.nextInt();
-		
+		int sizeX = scan.nextInt();
+
 		Point start = new Point();
 		start.x = scan.nextInt();
 		start.y = scan.nextInt();
-		
+
 		Point exit = new Point();
 		exit.x = scan.nextInt();
 		exit.y = scan.nextInt();
-		
+
 		int maze[][] = new int[sizeY][sizeX];
-		
-		for(int i = 0; i < sizeY; i++)
+
+		for (int i = 0; i < sizeY; i++)
 			for (int j = 0; j < sizeX; j++)
 				maze[i][j] = scan.nextInt();
 
 		scan.close();
-		
+
 		// 아래 코드를 작성하시오.
-		
+
 		int[][] mark = new int[sizeY][sizeX];
-		for(int i = 0; i < sizeY; i++)
+		for (int i = 0; i < sizeY; i++)
 			for (int j = 0; j < sizeX; j++)
 				mark[i][j] = 0;
 
@@ -70,7 +81,7 @@ public class Assignment5_3 {
 		// initialize stk
 		{ // push
 			Status status = new Status();
-			
+
 			status.x = start.x;
 			status.y = start.y;
 			status.position = 0;
@@ -81,26 +92,51 @@ public class Assignment5_3 {
 		mark[start.y][start.x] = 1;
 
 		int x, y, position;
-		while(top != -1) {
+		while (top != -1) {
 			{ // pop
 				x = stk[top].x;
 				y = stk[top].y;
 				position = stk[top].position;
 			} top--;
-			
+
 			while (position <= 3) {
 				System.out.println("for position " + position);
 				Point next = new Point();
-				int[][] move = { {1, 0}, {-1, 0}, {0, 1}, {0, -1} };
-				next.x = x + move[position][0];
-				next.y = y + move[position][1];
+				// int[][] move = { {1, 0}, {-1, 0}, {0, 1}, {0, -1} };
+				// next.x = x + move[position][0];
+				// next.y = y + move[position][1];
 
-				// next.x = x + xMove(position);
-				// next.y = y + yMove(position);
+				next.x = x + xMove(position);
+				next.y = y + yMove(position);
 
 				if (next.x == exit.x && next.y == exit.y) {
 					System.out.println("The path is as follows.");
 					// 출력 형식
+					maze[y][x] = 2;
+					while (top > -1) { // while(!isEmpty());
+						{ // pop
+							x = stk[top].x;
+							y = stk[top].y;
+							position = stk[top].position;
+						} top--;
+						maze[y][x] = 2;
+					}
+
+					for (int i = 0; i < sizeY; i++) {
+						for (int j = 0; j < sizeX; j++) {
+							if (j == start.x && i == start.y) {
+								System.out.print("S ");
+							} else if (j == exit.x && i == exit.y) {
+								System.out.print("F ");
+							} else if (maze[i][j] == 2) {
+								System.out.print("* ");
+							} else {
+								System.out.print(maze[i][j] + " ");
+							}
+						}
+						System.out.println();
+					}
+
 					System.out.println((next.x) + " " + (next.y));
 					return;
 				}
@@ -109,11 +145,11 @@ public class Assignment5_3 {
 					mark[next.y][next.x] = 1;
 					{ // push
 						Status status = new Status();
-						
+
 						status.x = x;
 						status.y = y;
 						status.position = 0;
-			
+
 						stk[++top] = status;
 						System.out.println("push: " + x + " " + y + " " + position);
 					}
@@ -122,7 +158,7 @@ public class Assignment5_3 {
 					y = next.y;
 					position = 0;
 					System.out.println("Goto " + x + " " + y);
-					
+
 				} else {
 					position += 1;
 				}
